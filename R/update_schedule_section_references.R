4d1452363233eba11a9a054a0165230542c24726
Daniel Chapman <daniel.chapman@energy.loc>
2017-12-04 10:10:13 -0800

    comma error

--- R/update_schedule_section_references.R
+++ R/update_schedule_section_references.R
@@ -20,7 +20,7 @@
 update_schedule_section_references <- function(building_subcategory){
   file <- str_split(read_file("sections/02_Schedules.idf"),  "\r\n")[[1]]
   target_lines <- which(str_detect(file, "Schedule:File")) + 3
-  file[target_lines] <- str_c("    $schedule  !- Name of File")
+  file[target_lines] <- str_c("    $schedule,  !- Name of File")
   #file[target_lines] <- str_c("../../schedules/", building_subcategory, ".csv, !- Name of File")
   #file[target_lines] <- str_c("C:\\Users\\daniel.chapman\\Documents\\2016-11-08 Load Shapes 1631\\2017-11-29 residential FCZ12 update\\schedules\\", building_subcategory, ".csv, !- Name of File")
   file <- str_c(file, collapse = "\r\n")

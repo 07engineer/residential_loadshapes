dc7d3b35626536c9f8971d468074d6c57b16df26
Daniel Chapman <daniel.chapman@energy.loc>
2017-12-05 15:22:40 -0800

    backstepping to constant setpoints

--- R/change_setpoint_schedules.R
+++ R/change_setpoint_schedules.R
@@ -1,6 +1,6 @@
 #' Change Setpoint Schedules
 #'
-#' Replaces schedule columns with ones in a research schedule file
+#' Replaces schedule columns with calculated values
 #'
 #'
 #' @param building_subctegory Building Subcategory
@@ -12,7 +12,26 @@
 
 
 change_setpoint_schedules <- function(building_subcategory, research_schedule_name){
-subcategory_schedule <- read_csv(str_c("schedules/", building_subcategory, ".csv"))
+  
+  energyplus_schedule <- read_csv("Residential_sch.csv") %>%
+    mutate(date = as_date(mdy_hm(Schedule)))
+  
+  energyplus_start_date = min(energyplus_schedule$date)
+  energyplus_end_date   = max(energyplus_schedule$date)
+
+analytic_schedule <- read_csv(str_c(analytic_schedule_path, analytic_schedule_file, sep = "/")) %>% 
+  # select(which(names(analytic_schedule) %in% names(energyplus_schedule))) %>% 
+  filter(date >= energyplus_start_date, date <= energyplus_end_date)
+
+
+analytic_schedule$cdd_base <- as.numeric(analytic_schedule$cdd_base)
+normalized_analytic_cdd_changepoints <- (analytic_schedule$cdd_base - mean(analytic_schedule$cdd_base)) / sd(analytic_schedule$cdd_base)
+setpoint_mean <- 23 # degrees Celsius
+setpoint_sd <- 3 # degrees Celsius
+heat_set_gap <- 5 # degrees Celsius
+energyplus_schedule$Cooling <- normalized_analytic_cdd_changepoints * setpoint_sd + setpoint_mean
+energyplus_schedule$Heating <- energyplus_schedule$Cooling - heat_set_gap
+
 
 setpoint_research_schedule <- read_csv(str_c("schedules/", "setpoint_research.csv"))
 
--- R/update_schedule.R
+++ R/update_schedule.R
@@ -3,22 +3,19 @@
 #' The analytic models produce schedules. We need to update the EnergyPlus schedule files with the new end-uses.
 #'
 #'
-#' @param analytic_schedule_path Path to the analytical schedule files
-#' @param analytic_schedule_filename Name of the file, including .csv
-#' @param run_name Name of run
 #' @keywords EnergyPlus, Parametrics, Calibration
 #' @export
 #' @examples
 #' update_schedule("L:/P/1631/Schedules/Final", "FCZ12_ALL_Usage_Level_LOW_GAS_Schedules.csv")
 
-update_schedule <- function(analytic_schedule_path, analytic_schedule_filename, run_name){
+update_schedule <- function(){
   energyplus_schedule <- read_csv("Residential_sch.csv") %>%
     mutate(date = as_date(mdy_hm(Schedule)))
   
   energyplus_start_date = min(energyplus_schedule$date)
   energyplus_end_date   = max(energyplus_schedule$date)
 
-  analytic_schedule <- read_csv(str_c(analytic_schedule_path, analytic_schedule_filename, sep = "/")) %>% 
+  analytic_schedule <- read_csv(str_c(analytic_schedule_path, analytic_schedule_file, sep = "/")) %>% 
     # select(which(names(analytic_schedule) %in% names(energyplus_schedule))) %>% 
     filter(date >= energyplus_start_date, date <= energyplus_end_date)
   
@@ -31,28 +28,37 @@ update_schedule <- function(analytic_schedule_path, analytic_schedule_filename, 
   do_not_update <- c("date", "hdd_base", "cdd_base")
   cols_to_update <- names(energyplus_schedule)[names(energyplus_schedule) %in% names(analytic_schedule)]
   cols_to_update <- cols_to_update[!(cols_to_update %in% do_not_update)]
-  analytic_schedule <- analytic_schedule[, cols_to_update]
+  #analytic_schedule <- analytic_schedule[, cols_to_update]
   
-  #Check that no schedule values are outside zero and one
-  fix_schedule_bounds <- function(vector){
-    if(max(vector) > 1){
-      warning("Analytic schedule had values > 1. They were set to 1.")
-      vector[vector > 1] <- 1
-    }
-    if(min(vector) < 0){
-      warning("Analytic schedule had values < 0. They were set to 0.")
-      vector[vector < 0] <- 0
-    }
-    return(vector)
-  }
-
-  analytic_schedule <- as_tibble(apply(analytic_schedule, 2, fix_schedule_bounds))
+  # #Check that no schedule values are outside zero and one
+  # fix_schedule_bounds <- function(vector){
+  #   if(max(vector) > 1){
+  #     warning("Analytic schedule had values > 1. They were set to 1.")
+  #     vector[vector > 1] <- 1
+  #   }
+  #   if(min(vector) < 0){
+  #     warning("Analytic schedule had values < 0. They were set to 0.")
+  #     vector[vector < 0] <- 0
+  #   }
+  #   return(vector)
+  # }
+  # 
+  # analytic_schedule <- as_tibble(apply(analytic_schedule, 2, fix_schedule_bounds))
   
   # Remember not to change the column order of the EnergyPlus file columns! They are referenced in the .idf file
   energyplus_schedule[, cols_to_update] <- analytic_schedule[, cols_to_update]
   
+  # Adjust the setpoint schedules
+  analytic_schedule$cdd_base <- as.numeric(analytic_schedule$cdd_base)
+  normalized_analytic_cdd_changepoints <- (analytic_schedule$cdd_base - mean(analytic_schedule$cdd_base)) / sd(analytic_schedule$cdd_base)
+  setpoint_mean <- 23 # degrees Celsius
+  setpoint_sd <- 3 # degrees Celsius
+  heat_set_gap <- 5 # degrees Celsius
+  energyplus_schedule$Cooling <- normalized_analytic_cdd_changepoints * setpoint_sd + setpoint_mean
+  energyplus_schedule$Heating <- energyplus_schedule$Cooling - heat_set_gap
+  
   #write_csv(energyplus_schedule, "Residential_sch.csv")
-  write_csv(energyplus_schedule, str_c("schedules/", run_name, ".csv"))
+  write_csv(energyplus_schedule, str_c("schedules/", building_subcategory, ".csv"))
 }
 
 

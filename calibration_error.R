a023464069d10b543dad6ca42b86cce01c7a8245
Daniel Chapman <daniel.chapman@energy.loc>
2018-01-05 16:05:21 -0800

    Tackling climate zones 1-11. Added text feedback for progress in master file. In calibration_error, set up for looping through multiple building subcategories in one climate zone

--- calibration_error.R
+++ calibration_error.R
@@ -1,74 +1,91 @@
 
+sim_path = "L:/P/1631/Task 4 - Baseline Profiles/Residential Calibrated Models 09182017/FCZ1_FCZ11/FCZ1"
 
-sim_path = "L:/P/1631/Task 4 - Baseline Profiles/Residential Calibrated Models 09182017/2017-11-29 residential FCZ12 update/FCZ12_ALL_GAS_LOW"
-#sim_path <- ("~/2016-11-08 Load Shapes 1631/2017-11-29 residential FCZ12 update/EP_input_batch")
+# sim_path = "L:/P/1631/Task 4 - Baseline Profiles/Residential Calibrated Models 09182017/2017-11-29 residential FCZ12 update/FCZ12_ALL_GAS_LOW"
+# sim_path <- ("~/2016-11-08 Load Shapes 1631/2017-11-29 residential FCZ12 update/EP_input_batch")
 
 sim_files <- dir(sim_path)[which(str_detect(dir(sim_path), "Meter"))]
 
-zone = 12
+# Figure out unique building_subcategories in folder
+subcategories <- str_replace_all(sim_files, "[[0-9]]", "") %>%
+  str_replace("FCZ_", "") %>%
+  str_replace("-RunMeter.csv", "") %>%
+  unique()
 
-# Read in billing data file 
+zone = str_split(sim_files[1], "_")[[1]][1]
+
+# Read in billing data file
 billing_data_path <- "L:/P/1631/Task 4 - Baseline Profiles/Residential Pre-Processor 091417/Data/Cleaned Data/engineering"
-billing_data <- read_csv(str_c(billing_data_path, "/FCZ", zone, ".csv")) %>%
+billing_data <- read_csv(str_c(billing_data_path, "/", zone, ".csv")) %>%
   mutate(date = ymd_hms(str_c(date, " ", hour, ":00:00")))
 
 # i = 1
+# j = 2
 
-RMSE_all <- vector()
-RMSE_peaks <- vector()
-identifier <- vector()
-runs <- vector()
-for(i in 1:length(sim_files)){
+parametrics <- read_csv("parametric_values.csv")
+
+for(j in 1:length(subcategories)){
+  building_subcategory <- subcategories[j]
+  subcategory_files <- sim_files[grep(building_subcategory, sim_files)]
+  cat("Calculating error matrix for subcategory ", building_subcategory, ".\n")
+
+  RMSE_all <- vector()
+  RMSE_peaks <- vector()
+  identifier <- vector()
+  runs <- vector()
+
+  for(i in 1:length(subcategory_files)){
     # Pick apart the simulation file name to get building subcategory for meter data filter
-    run_elements <- str_split(str_replace(sim_files[i], "Meter.csv", ""), "_")[[1]]
-    #zone <- as.integer(str_replace_all(run_elements[1], "[[:alpha:]]", "")) # I need this outside the loop for speed. 
-    level <- str_replace(run_elements[4], "-Run", "")%>% 
+    run_elements <- str_split(str_replace(subcategory_files[i], "Meter.csv", ""), "_")[[1]]
+    #zone <- as.integer(str_replace_all(run_elements[1], "[[:alpha:]]", "")) # I need this outside the loop for speed.
+    level <- str_replace(run_elements[4], "-Run", "")%>%
       str_replace_all("[:digit:]", "")
     type <- run_elements[2]
     fuel <- run_elements[3]
     run <- str_c("Run", str_c(str_extract_all(run_elements[4], "[:digit:]")[[1]], collapse = ""))
-    
+
     sim <- read_EP_Output(str_c(sim_path, sim_files[i], sep = "/")) %>%
       rename(kW_sim = `ElectricityNet:Facility`)
-    
+
     results <- billing_data %>%
-      filter(usage_level == level, 
-             heat_fuel == fuel, 
+      filter(usage_level == level,
+             heat_fuel == fuel,
              iou_building_type == type) %>%
       select(kW_bills = kW) %>%
       bind_cols(select(sim, date, kW_sim)) %>%
       select(date, everything()) %>%
-      mutate(kW_bills_norm  = kW_bills / sum(kW_bills), 
+      mutate(kW_bills_norm  = kW_bills / sum(kW_bills),
              kW_sim_norm = kW_sim / sum(kW_sim))
-    
-    identifier[i] <- str_c(type, fuel, level, sep = "_")  
+
+    identifier[i] <- str_c(type, fuel, level, sep = "_")
     RMSE_all[i] <- sqrt(mean((results$kW_sim_norm - results$kW_bills_norm)^2))
-    
-    # peaks <- results %>% 
+
+    # peaks <- results %>%
     #   filter(kW_bills > summary(results$kW_bills)["3rd Qu."])
-    
-    peaks <- results %>% 
+
+    peaks <- results %>%
       filter(kW_bills > 0.8 * max(kW_bills))
-    
+
     RMSE_peaks[i] <- sqrt(mean((peaks$kW_sim_norm - peaks$kW_bills_norm)^2))
     runs[i] <- run
-    
+
     cat(sim_files[i], "\n")
-} # for loop
+  } # for loop
 
-parametrics <- read_csv("parametric_values.csv")
 
-batch_errors <- tibble(file = sim_files, 
-                       subsector = identifier, 
-                       error_all = RMSE_all, 
-                       error_peak = RMSE_peaks, 
-                       sufflist = runs) %>% 
+
+
+batch_errors <- tibble(file = subcategory_files,
+                       subsector = identifier,
+                       error_all = RMSE_all,
+                       error_peak = RMSE_peaks,
+                       sufflist = runs) %>%
   left_join(parametrics)
 
-write.csv(batch_errors, "batch_errors.csv")
+write.csv(batch_errors, str_c(zone, "/errors/", building_subcategory,"_errors.csv"))
+}
 
 
-
-# min_batch_errors <- batch_errors %>% 
-  # group_by(subsector) %>% 
-  # summarise(min_error = min(error), best_run = file[which(error == min(error))])
+# min_batch_errors <- batch_errors %>%
+# group_by(subsector) %>%
+# summarise(min_error = min(error), best_run = file[which(error == min(error))])
--- master.R
+++ master.R
@@ -25,30 +25,33 @@ analytic_schedule_path <- "L:/P/1631/Task 4 - Baseline Profiles/Residential Pre-
 coefficients_path <- "L:/P/1631/Task 4 - Baseline Profiles/Residential Pre-Processor 091417/Data/Constrained Regression Coefficients"
 pre_processor_path = "C:\\EnergyPlusV8-5-0\\PreProcess\\ParametricPreProcessor\\parametricpreprocessor"
 
+
+# For debugging or individual runs:
+# family = "MULTIFAMILY" # "SINGLEFAMILY" or "MULTIFAMILY" , or "ALL" for FCZ12
+# fuel = "ELECTRIC" # Must be "GAS" or "ELECTRIC"
+# size = "HIGH"  #Must be "LOW", "MEDIUM", "HIGH"
+# climate_zone = "FCZ2"
+
 # Clean out batch folder if you want
 #batch_files_to_delete <- dir("EP_input_batch")[!dir("EP_input_batch") %in% "RunDirMulti.bat"]
 #file.remove(str_c("EP_input_batch", batch_files_to_delete, sep = "/"))
 
-# For debugging or individual runs:
-family = "MULTIFAMILY" # "SINGLEFAMILY" or "MULTIFAMILY" , or "ALL" for FCZ12
-fuel = "ELECTRIC" # Must be "GAS" or "ELECTRIC"
-size = "HIGH"  #Must be "LOW", "MEDIUM", "HIGH"
-climate_zone = "FCZ1"
+# For batch runs
+families = c("SINGLEFAMILY", "MULTIFAMILY")
+fuels = c("GAS", "ELECTRIC")
+sizes = c("LOW", "MEDIUM", "HIGH")
+climate_zones = str_c("FCZ", 7:11)
 
-# # For batch runs
-# families = c("SINGLEFAMILY", "MULTIFAMILY")
-# fuels = c("GAS", "ELECTRIC")
-# sizes = c("LOW", "MEDIUM", "HIGH")
-# climate_zones = str_c("FCZ", 2:11)
-#
-# for(i in seq_along(families)){
-# for(j in seq_along(fuels)){
-# for(k in seq_along(sizes)){
-# for(m in seq_along(climate_zones)){
-#   family = families[i]
-#   fuel = fuels[j]
-#   size = sizes[k]
-#   climate_zone = climate_zones[m]
+for(m in seq_along(climate_zones)){
+  climate_zone = climate_zones[m]
+  batch_folder <- climate_zone
+
+for(i in seq_along(families)){
+for(j in seq_along(fuels)){
+for(k in seq_along(sizes)){
+  family = families[i]
+  fuel = fuels[j]
+  size = sizes[k]
 
   if(family == "SINGLEFAMILY" & fuel == "GAS") {
     file_00 <- "00_SF_Model_gasfurnace_crawlspace.idf"
@@ -65,6 +68,7 @@ climate_zone = "FCZ1"
   }
 
 building_subcategory = str_c(climate_zone, family, fuel, size, sep = "_")
+cat("\nBUILDING SUBCATEGORY: ", building_subcategory, "\n")
 analytic_schedule_file <- str_c(climate_zone, family,"Usage_Level", size, fuel, "Schedules.csv", sep = "_")
 coefficients_file <- str_c(climate_zone, family,"Usage_Level", size, fuel, "coefficients.csv", sep = "_")
 
@@ -75,55 +79,61 @@ file.remove(str_c("EP_output", dir("EP_output"), sep = "/"))
 #### Update Schedule Section References #
 #########################################
 
+cat("Updating schedule references in .idf file. \n")
 update_schedule_section_references(building_subcategory)
 
 #########################################
 #### Update Enduse Coefficients #########
 #########################################
 
+cat("Updating end use coefficients in .idf file. \n")
 update_enduse_coefficients(coefficients_path, coefficients_file, file_00)
 
 #########################################
 #### Combine Sections ###################
 #########################################
 
+cat("Combining sections of .idf file. \n")
 combine_sections(building_subcategory, file_00)
 
 #########################################
 #### Run Parametric Pre-processor #######
 #########################################
 
+cat("Running parametric pre-processor. \n")
+
 files_before_PP = dir("EP_input")
 system(str_c(pre_processor_path, " EP_input", "\\", building_subcategory, ".idf"))
 files_after_PP = dir("EP_input")
 runs <- files_after_PP %>%
   str_subset(str_c(building_subcategory, "-"))
-file.copy(str_c("EP_input", runs, sep = "/"), str_c("EP_input_batch_2", runs, sep = "/"), overwrite = TRUE)
+file.copy(str_c("EP_input", runs, sep = "/"), str_c(batch_folder, runs, sep = "/"), overwrite = TRUE)
 
 #########################################
 #### Update EnergyPlus Schedules ########
 #########################################
 
-# This can be done earlier as a separate script
-#update_schedule(analytic_schedule_path, analytic_schedule_file, building_subcategory)
-update_schedule()
+# cat("Update energy plus schedule with analytic schedule values. \n")
+# update_schedule()
 
 #########################################
 #### Update Setpoint Schedules ##########
 #########################################
 
+# cat("Change setpoint schedule. \n")
 # change_setpoint_schedules(anamoly_changepoint = 70, max_drop = 20, cooling_setpoint = 24, heating_set_delta = 2)
 
-# }
-# }
-# }
-# }
+}
+}
+}
+}
 
 #########################################
 #### Update RunEPlus.bat         ########
 #########################################
 
-update_RunEPlus_batch()
+#cat("Update batch file")
+#update_RunEPlus_batch()
 
 
 
